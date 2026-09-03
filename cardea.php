fa5db92dd14b78e9ac734926568f8129fb90f24d
omikheev <omikheev@gmail.com>
2026-09-03 18:19:11 -0400

    Release 1.0.2: version bump, changelog and upgrade notice

--- cardea.php
+++ cardea.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Cardea - Proof-of-Work Comment Spam Protection
  * Plugin URI:        https://olegmikheev.com/cardea
  * Description:       Lightweight, zero-dependency Proof-of-Work anti-spam protection for WordPress comments. Uses client-side cryptographic mining to filter out automated spam.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Author:            Oleg Mikheev
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'CARDEA_VERSION', '1.0.1' );
+define( 'CARDEA_VERSION', '1.0.2' );
 define( 'CARDEA_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'CARDEA_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'CARDEA_DEFAULT_DIFFICULTY', 4 );
